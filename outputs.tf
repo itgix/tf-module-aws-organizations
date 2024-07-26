243ffdc3e6cdc198e931d6967b13815423392dff
Boris Yakimov <boris.yakimov@itgix.com>
2024-07-26 15:46:33 +0300

    #50 update output params

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
 # TODO: can this be taken from some form of mapping of account name to id to make those more reliable
+# TODO: verify that this returns correct account ids
 output "shared_services_account_id" {
   value = aws_organizations_organizational_unit.main.accounts[0].id
+}
+
+output "audit_account_id" {
+  value = aws_organizations_organizational_unit.main.accounts[1].id
 }
 
 output "dev_account_id" {
