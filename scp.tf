5fa593bda51b44258b85b0b5dba013349c98d986
Boris Yakimov <boris.yakimov@itgix.com>
2024-10-22 11:49:56 +0300

    #60 add region policy

--- scp.tf
+++ scp.tf
@@ -2,39 +2,21 @@
 # Service Control Policies
 ####################################
 
-resource "aws_organizations_policy" "shared_policy" {
+# DynamoDB doesn't support ABAC so we cannot restrict creating dynamodb tables without tags
+# RDS has a bug with AWS console where when you create an RDS cluster with tags, its tags are not propagated down to the RDS instance and if we have an SCP that restricts creation of RDS instances without tags, we encounter this problem where an RDS aurora cluster cannot be created from AWS console, it does not affect creating an RDS instance, and it also does not affect creating an RDS cluster from SDK or CLI
+resource "aws_organizations_policy" "tagging_policy" {
   content     = templatefile("${path.module}/scp-policies/tagging-policy.json", {})
-  name        = "Shared SCP"
+  name        = "Tagging Policy"
   type        = "SERVICE_CONTROL_POLICY"
-  description = "SCPs shared for all environments"
+  description = "SCP that enforces having mandatory tags on resources"
 }
 
-## TODO: add below requirements in the policy
-# {
-#   "Sid": "DenyCreateSecretWithNoCostCenterTag",
-#   "Effect": "Deny",
-#   "Action": "secretsmanager:CreateSecret",
-#   "Resource": "*",
-#   "Condition": {
-#     "Null": {
-#       "aws:RequestTag/CostCenter": "true"
-#     }
-#   }
-# },
-# {
-#   "Sid": "DenyRunInstanceWithNoCostCenterTag",
-#   "Effect": "Deny",
-#   "Action": "ec2:RunInstances",
-#   "Resource": [
-#     "arn:aws:ec2:*:*:instance/*",
-#     "arn:aws:ec2:*:*:volume/*"
-#   ],
-#   "Condition": {
-#     "Null": {
-#       "aws:RequestTag/CostCenter": "true"
-#     }
-#   }
-# }
+resource "aws_organizations_policy" "region_policy" {
+  content     = templatefile("${path.module}/scp-policies/region-policy.json", {})
+  name        = "Region Policy"
+  type        = "SERVICE_CONTROL_POLICY"
+  description = "SCP that restricts access to allowed AWS regions"
+}
 
 # resource "aws_organizations_policy" "ou_prod_policy" {
 #   content     = templatefile("${path.module}/scp-policies/ou-prod-scp-policies.json", {})
