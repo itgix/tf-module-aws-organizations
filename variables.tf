802af947be5c9c096928270f541825ffa5fa8f89
Monika Radeva <monika.radeva@itgix.com>
2025-06-24 14:37:58 +0000

    Feature/#52 central backup

--- variables.tf
+++ variables.tf
@@ -8,8 +8,7 @@ variable "enabled_policy_types" {
   description = "Enabled policy types for the organization"
 }
 
-
-####################### NEW ORG MODULE ###########################
+# Accounts to create under each OU
 variable "accounts" {
   description = "Map of accounts grouped by OU"
   type = map(map(object({
@@ -18,7 +17,7 @@ variable "accounts" {
   default = {}
 }
 
-# ################### Map for policy attachment ###################
+# Attach SCPs to OUs
 variable "scp_attachments" {
   description = "List of SCP policy to OU attachment objects"
   type = list(object({
@@ -28,6 +27,7 @@ variable "scp_attachments" {
   default = []
 }
 
+# Attach tag policies to root, OUs, or accounts
 variable "tag_policy_attachments" {
   description = "List of tag policy attachments to root, OUs, or accounts"
   type = list(object({
@@ -36,3 +36,34 @@ variable "tag_policy_attachments" {
     target_key  = string # key from corresponding resource maps
   }))
 }
+
+variable "management_account_id" {
+  description = "AWS Organizations management account ID"
+  type        = string
+}
+
+# List of delegated service admins
+variable "delegated_admins" {
+  type = list(object({
+    account_id        = string
+    service_principal = string
+  }))
+  description = "Accounts to register as delegated admins for services"
+}
+
+
+# Enable backup-specific policy
+variable "include_backup_policy" {
+  description = "Whether to attach a backup-specific Organizations policy"
+  type        = bool
+  default     = false
+}
+
+
+# Delegated admin for backup
+variable "backup_admin_account_id" {
+  description = "Delegated admin account ID for backup service"
+  type        = string
+  default     = ""
+}
+
